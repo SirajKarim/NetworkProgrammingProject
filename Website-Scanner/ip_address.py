6bfad4085260dbd705bf5f368a20af405a0cf176
SirajKarim <msirajakarim437@gmail.com>
2020-04-27 01:57:41 +0500

    Website Scannig

---
+++ Website-Scanner/ip_address.py
@@ -0,0 +1,13 @@
+import os
+
+
+def get_ip_address(domain):
+    command = "host " + str(domain)
+
+    process = os.popen(command)
+
+    # Getting the IP from the output
+    result = str(process.read())
+    marker = result.find("has address") + 12
+    return result[marker:].splitlines()[0]
+
